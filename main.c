be46b0f878142596c91a59c50e154f61570a390a
koaji <koajisasi23@gmail.com>
2019-08-18 20:55:35 +0900

    ベースプログラムの追加
    ベースとなるプログラムを追加．

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "header.h"
+
+int main(int argc, char **argv){
+
+    if(argc < 2){
+        fprintf(stderr,"Usage: command <csv file>\n");
+        return -1;
+    }
+    
+    return 0;
+}
